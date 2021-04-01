4917244955a2f8e439239c5d0d3f1931639fc6de
RuiMargarido <rp.margarido@campus.fct.unl.pt>
2021-04-01 16:46:28 +0100

    plz

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     # output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(NADA CABRAO))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format("NADA CABRAO"))
 
 
 if __name__ == "__main__":
