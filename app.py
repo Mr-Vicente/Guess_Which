48b317d1fc0c978c03e7cde270ac2ec312e87947
RuiMargarido <rp.margarido@campus.fct.unl.pt>
2021-04-01 16:36:21 +0100

    Apaguei a ref ao model so para correr

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,11 +16,11 @@ def predict():
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    # prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    # output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(NADA CABRAO))
 
 
 if __name__ == "__main__":
