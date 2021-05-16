fa77a92329a4cb4b5a590f537d91f7cd4690069a
Mariana Bonito <mariana.f.bonito@gmail.com>
2021-05-16 20:18:10 +0100

    dificulties done!!!

--- knn_images.py
+++ knn_images.py
@@ -2,26 +2,29 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-
+import math
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import torchvision.models as models
 import torch
 import random
 
 DATA_DIR = "./assets/images"
 
 
-def load_vgg():
+def load_resnet():
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg11', pretrained=True)
     # or any of these variants
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg11_bn', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg13', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg13_bn', pretrained=True)
-    model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg16', pretrained=True)
+    # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg16', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg16_bn', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg19', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.9.0', 'vgg19_bn', pretrained=True)
+    model = models.resnet152(pretrained=True)
     model.eval()
+
     return model
 
 
@@ -56,7 +59,7 @@ def create_images_encoding(vgg, data_loader):
 
 
 def L2_NORM(img_enc_sel, img_enc_inf):
-    return ((img_enc_sel-img_enc_inf)**2).sum(axis=0)
+    return math.sqrt(((img_enc_sel-img_enc_inf)**2).sum(axis=0))
 
 
 def find_topK_similar(data_loader, encodings, image_index, k=5):
@@ -84,27 +87,76 @@ def find_topK_similar(data_loader, encodings, image_index, k=5):
 
 def find_topK_similar_simple(encodings, image_index, k=5, difficulty=1):
     distances = []
-    k += 1
+    sel_img_encoding = torch.tensor(encodings[image_index])
     for image_idx in range(encodings.shape[0]):
-        sel_img_encoding = torch.tensor(encodings[image_index])
         curr_img_encoding = torch.tensor(encodings[image_idx])
         distance = L2_NORM(sel_img_encoding, curr_img_encoding)
         if torch.cuda.is_available():
             distance = distance.detach().cpu().numpy()
         distances.append(distance)
 
-    span = int(random.random() * difficulty) * 10
-    range_ = span + k
-    distances_topK_indicies = np.argpartition(distances, range_)[:range_]
-    random_indicies = np.array(list(range(range_)))
-    np.random.shuffle(random_indicies)
-    y = [random_indicies[i] for i in range(k)]
+    return difficulty_img_selection(k,difficulty,np.array(distances))
 
-    indeces = np.array(y)
-    distances_topK_indicies = distances_topK_indicies[indeces]
-    distances_topK_indicies = np.delete(distances_topK_indicies, np.where(distances_topK_indicies == image_index))
-    distances = np.array(distances)
-    return distances[distances_topK_indicies], distances_topK_indicies
+def difficulty_img_selection(k, difficulty, distances):
+    range_inf_idx = []
+    range_sup_idx = []
+    x1 = 20.0
+    x2 = 22.0
+    x3 = 22.75
+    x4 = 24.0
+
+    dist_size = distances.shape[0]
+    indices = np.argsort(distances)
+    distances_ordered = distances[indices]
+
+    '''plt.hist(distances_ordered)
+    plt.title('Image Distances from Image {}'.format(chosen_id))
+    plt.xlabel('Distance between images')
+    plt.ylabel('Amount of images with that distance')
+    plt.savefig('distance_distribution/dist_chosen_id_{}.png'.format(chosen_id))'''
+
+    if difficulty == 1: #easy
+        lim_inf_inf = 0
+        lim_inf_sup = 0
+        lim_sup_inf = x2
+        lim_sup_sup = x3
+    if difficulty == 2: #medium
+        lim_inf_inf = x1
+        lim_inf_sup = x2
+        lim_sup_inf = x3
+        lim_sup_sup = x4
+    if difficulty == 3: #hard
+        lim_inf_inf = 0
+        lim_inf_sup = x1
+        lim_sup_inf = x4
+        lim_sup_sup = distances_ordered[dist_size-1]
+
+    dist_aux = []
+    for idx in range(distances.shape[0]):
+        dist_idx = distances[idx]
+        if dist_idx > 0  and not (dist_idx in dist_aux):
+            if lim_inf_inf <= dist_idx < lim_inf_sup:
+                dist_aux.append(dist_idx)
+                range_inf_idx.append(idx)
+            elif lim_sup_inf <= dist_idx < lim_sup_sup:
+                range_sup_idx.append(idx)
+                dist_aux.append(dist_idx)
+    if len(range_inf_idx) < k and len(range_inf_idx) < len(range_sup_idx):
+        difficulty_range_idx = np.array(range_sup_idx)
+    else:
+        difficulty_range_idx = np.array(range_inf_idx)
+    #difficulty_range_idx = np.append(np.array(range_inf_idx), np.array(range_sup_idx)).astype(int)  # por algum motivo estava a transformar num vetor de floats
+    if len(difficulty_range_idx) == 0:
+        _, difficulty_range_idx = difficulty_img_selection(k, difficulty + 1, distances) # nunca vai ser chamado no quando se esta no nivel dificil
+    np.random.shuffle(difficulty_range_idx)
+    difficulty_range_idx = difficulty_range_idx[:k]
+
+    print("Range Inf: {}".format(range_inf_idx))
+    print("Range Sup: {}".format(range_sup_idx))
+    print("Range Total: {}".format(difficulty_range_idx))
+    print("Presented distances: {}".format(distances[difficulty_range_idx]))
+
+    return distances[difficulty_range_idx], difficulty_range_idx
 
 
 def obtain_similiar_images(top_k_indicies, chosen_idx):
@@ -145,7 +197,7 @@ def prepare_encodings():
 
     return encodings
 
-def prepare_encodings_vgg():
+def prepare_encodings_resnet():
     feats_dir = "./assets/feats_a"
     encodings = None
     for enc_name in sorted(os.listdir(feats_dir)):
@@ -160,13 +212,13 @@ def prepare_encodings_vgg():
 
 
 def get_nearest_images_idx(chosen_idx, difficulty=1):
-    vgg = load_vgg()
+    resnet = load_resnet()
     if torch.cuda.is_available():
-        vgg.to('cuda')
+        resnet.to('cuda')
 
     # data_loader = loading_data()
     # encodings = create_images_encoding(vgg, data_loader)
-    encodings = prepare_encodings_vgg()
+    encodings = prepare_encodings_resnet()
     print(f'Chosenid: {chosen_idx}')
     print(f'Encoding shape: {encodings.shape}')
     _, top_k_indicies = find_topK_similar_simple(encodings, chosen_idx, k=5, difficulty=difficulty)
